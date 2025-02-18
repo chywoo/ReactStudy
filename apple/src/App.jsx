7af68ab209eec4c0e0d3a996b436005a5554fca1
Sungho Park <chywoo@gmail.com>
2025-02-18 16:37:40 -0500

    Add new commpoent called 'Model' This is for studying Three.js

--- apple/src/App.jsx
+++ apple/src/App.jsx
@@ -1,7 +1,7 @@
 import Hero from './components/Hero'
 import Highlights from './components/Highlights'
 import Navbar from './components/Navbar'
-import VideoPlayer from './components/Test'
+import Model from './components/Model'
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
       <Navbar />
       <Hero />
       <Highlights />
+      <Model />
     </main>
   )
 }
